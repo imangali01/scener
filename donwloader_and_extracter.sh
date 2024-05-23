ef2dd902599baabf2dd8578a4bee418909328eee
Imangali <65546538+imangali01@users.noreply.github.com>
2024-05-23 22:48:44 +0500

    Update donwloader_and_extracter.sh

--- donwloader_and_extracter.sh
+++ donwloader_and_extracter.sh
@@ -3,11 +3,13 @@
 # Download the tar files
 for n in {0..19}
 do
+    echo 'downloading scener_'$n'.tar...'
     wget https://github.com/imangali01/scener-dataset/releases/download/v1.0/scener_${n}.tar
 done
 
 # Extract the tar files
 for n in {0..19}
 do
+    echo 'unpacking scener_'$n'.tar...'
     tar -zxf scener_${n}.tar
 done
