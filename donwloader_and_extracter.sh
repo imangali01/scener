6aa7c7e8dcc2ce0ead54ca4b2874c6bca10eb970
Imangali <65546538+imangali01@users.noreply.github.com>
2024-05-25 14:05:07 +0500

    Update donwloader_and_extracter.sh

--- donwloader_and_extracter.sh
+++ donwloader_and_extracter.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Default value
+num_files=19
+
+# Parse command-line argument
+while getopts "f:" opt; do
+  case $opt in
+    f) num_files=$OPTARG ;;
+    \?) echo "Invalid option -$OPTARG" >&2 ;;
+  esac
+done
+
 echo '[+] STARTING DOWNLOADING!'
 
 # Download the tar files
-for n in {0..19}
+for n in $(seq 0 $num_files)
 do
     echo '[+] DOWNLOADING scener_'$n'.tar...'
     wget https://github.com/imangali01/scener-dataset/releases/download/v1.0/scener_${n}.tar
@@ -13,7 +24,7 @@ echo '[+] COMPLETED DOWNLOADING!'
 echo '[+] STARTING UNPACKING!'
 
 # Extract the tar files
-for n in {0..19}
+for n in $(seq 0 $num_files)
 do
     echo '[+] UNPACKING scener_'$n'.tar...'
     tar -zxf scener_${n}.tar
