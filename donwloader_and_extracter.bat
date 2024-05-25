e5692c63e84617894380ae0d01b2210415007d8d
Imangali <65546538+imangali01@users.noreply.github.com>
2024-05-25 13:54:10 +0500

    Create donwloader_and_extracter.bat

---
+++ donwloader_and_extracter.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo [+] STARTING DOWNLOADING!
+
+REM Download the tar files
+for /l %%n in (0,1,19) do (
+    echo [+] DOWNLOADING scener_%%n.tar...
+    curl -L -O https://github.com/imangali01/scener-dataset/releases/download/v1.0/scener_%%n.tar
+)
+
+echo [+] COMPLEATED DOWNLOADING!
+echo [+] STARTING UNPACKING!
+
+REM Extract the tar files
+for /l %%n in (0,1,19) do (
+    echo [+] UNPACKING scener_%%n.tar...
+    tar -zxf scener_%%n.tar
+)
+
+echo [+] COMPLEATED!
+pause
